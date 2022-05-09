c9882f93365a298f8c3313bcf18c7f87f93501c0
Cheapsik <cheapsik@gmail.com>
2022-05-09 19:31:03 +0200

    feat: added initial component structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import DynamicDebitCard from './DynamicDebitCard';
+import DynamicDebitCard from './components/DynamicDebitCard/DynamicDebitCard.js';
+import './main.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
